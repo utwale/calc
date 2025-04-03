d7a6c9348bda074eb51f638816f06ec3a10b267d
utwale <utwalesakshi@gmail.com>
2025-04-03 15:20:42 +0530

    java program created

---
+++ addition.java
@@ -0,0 +1,13 @@
+import java.util.*,
+import java.lang.*;
+class Add{
+public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter first no: ");
+        System.out.println("Enter second no: ");
+	int a = sc.nextInt();
+	int b= sc.nextInt();
+	int sum = a+b;
+	System.out.println("Addition: "+sum);
+	sc.close();
+}}
